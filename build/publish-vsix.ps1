94f2938d6348feb3d95695aed40e469c310f03d7
Hossein Margani <hossein.margani@aqovia.com>
2023-05-24 09:53:43 +0100

    DO-674: Add publish to marketplace stage

---
+++ build/publish-vsix.ps1
@@ -0,0 +1,25 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()][String]$PersonalAccessToken
+)
+
+$VsixPath = "$PSScriptRoot\..\src\VisualStudio.ExtensionPack.22\bin\Release\Aqovia.VisualStudio.ExtensionPack.vsix"
+$ManifestPath = "$PSScriptRoot\extension-manifest.json"
+
+$VSInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" `
+    -latest -prerelease -format json | ConvertFrom-Json
+$VSInstallationPath = $VSInstallation.installationPath
+
+Write-Host "Visual Studio installation path: $VSInstallationPath"
+$VsixPublisher = Join-Path -Path $VSInstallationPath `
+    -ChildPath "VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe" `
+    -Resolve
+
+Write-Host "VsixPublisher path: $VsixPublisher"
+Test-Path $VsixPublisher
+
+& $VsixPublisher publish `
+    -payload $VsixPath `
+    -publishManifest $ManifestPath `
+    -personalAccessToken $PersonalAccessToken `
+    -ignoreWarnings "VSIXValidatorWarning01,VSIXValidatorWarning02,VSIXValidatorWarning08"
